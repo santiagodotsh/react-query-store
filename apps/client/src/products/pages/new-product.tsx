fe0a93dc7f06c2dc16f420ab1f3820bc04f2568d
Santiago Ramos <santiagodotsh@gmail.com>
2024-10-10 14:08:56 -0500

    update form with react-hook-form

--- apps/client/src/products/pages/new-product.tsx
+++ apps/client/src/products/pages/new-product.tsx
@@ -4,45 +4,139 @@ import {
   Input,
   Textarea
 } from '@nextui-org/react'
+import { Controller, useForm } from 'react-hook-form'
+import type { SubmitHandler } from 'react-hook-form'
+
+interface FormInputs {
+  title: string
+  price: number
+  description: string
+  category: string
+  image: string
+}
 
 export function NewProduct() {
+  const { control, handleSubmit, watch } = useForm<FormInputs>({
+    defaultValues: {
+      title: '',
+      price: 0,
+      description: '',
+      category: "men's clothing",
+      image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg'
+    }
+  })
+
+  const image = watch('image')
+  
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className='w-full flex-col'>
       <h1 className='text-2xl font-bold'>
         New product
       </h1>
 
-      <form className='w-full'>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='w-full'
+      >
         <div className='flex justify-around items-center'>
           <div className='flex-col w-[500px]'>
-            <Input className='mt-2' type='text' label='Product title' />
-            <Input className='mt-2' type='number' label='Product price' />
-            <Input className='mt-2' type='url' label='Product URL' />
+            <Controller
+              control={control}
+              name='title'
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  type='text'
+                  value={field.value}
+                  onChange={field.onChange}
+                  className='mt-2'
+                  label='Title'
+                />
+              )}
+            />
 
-            <Textarea className='mt-2' label='Product description' />
+            <Controller
+              control={control}
+              name='price'
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  type='number'
+                  value={field.value?.toString()}
+                  onChange={(e) => field.onChange(+e.target.value)}
+                  className='mt-2'
+                  label='Price'
+                />
+              )}
+            />
 
-            <select className='rounded-md p-3 mt-2 bg-gray-800 w-full'>
-              <option value="men's clothing">Men's clothing</option>
-              <option value="women's clothing">Women's clothing</option>
-              <option value='jewelery'>Jewelery</option>
-              <option value='electronics'>Electronics</option>
-            </select>
+            <Controller
+              control={control}
+              name='image'
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  type='url'
+                  value={field.value}
+                  onChange={field.onChange}
+                  className='mt-2'
+                  label='URL'
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name='description'
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Textarea
+                  value={field.value}
+                  onChange={field.onChange}
+                  className='mt-2'
+                  label='Description'
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name='category'
+              rules={{ required: true }}
+              render={({ field }) => (
+                <select
+                  value={field.value}
+                  onChange={field.onChange}
+                  className='rounded-md p-3 mt-2 bg-gray-800 w-full'
+                >
+                  <option value="men's clothing">Men's clothing</option>
+                  <option value="women's clothing">Women's clothing</option>
+                  <option value='jewelery'>Jewelery</option>
+                  <option value='electronics'>Electronics</option>
+                </select>
+              )}
+            />
 
             <br />
 
-            <Button className='mt-2' color='primary'>
+            <Button
+              type='submit'
+              className='mt-2'
+              color='primary'
+            >
               Create
             </Button>
           </div>
 
           <div
             className='bg-white rounded-2xl p-10 flex items-center'
-            style={{
-              width: '500px',
-              height: '600px'
-            }}
+            style={{ width: '400px' }}
           >
-            <Image src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg' />
+            <Image src={image} />
           </div>
         </div>
       </form>
--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -5,7 +5,7 @@ const { nextui } = require("@nextui-org/react");
 export default {
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
